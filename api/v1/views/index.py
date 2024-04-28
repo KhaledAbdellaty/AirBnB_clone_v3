1088748f390e12ce46608c8678c6f21c696f6556
saharrrrr <skmao23@gmail.com>
2024-04-28 05:12:55 -0700

    Status of API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,12 +8,21 @@ from models import storage
 
 @app_views.route('/status')
 def status_route():
+    """Returns the server status.
+
+    Returns:
+        JSON object with the current server status.
+    """
     status = {"status": "OK"}
     return jsonify(status)
 
 
 @app_views.route('/stats')
 def get_stats():
+    """Retrives the count of each object type.
+
+    Returns:
+        JSON object with the number of objects by type."""
     classes = [
            "Amenity", "City",
            "Place", "Review", "State", "User",
