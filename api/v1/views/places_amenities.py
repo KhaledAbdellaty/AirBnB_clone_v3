14ad74e5bed3e55b4507e4040b585218f4cab3e8
saharrrrr <skmao23@gmail.com>
2024-05-01 21:49:53 -0700

    add places_amenity file

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Defines the view for places amenities Api calls"""
+from flask import abort, jsonify, request
+from flasgger import swag_from
+from models import storage
+from models.amenity import Amenity
+from api.v1.views import app_views
+
+
+@app_views.route('/places/<place_id>/amenities', methods=['GET'])
+def get_place_amenities(place_id):
+    """getting a list of amenities
+    from a place on id.
+    """
+    place = storage.get("Place", place_id)
+    if place is None:
+        abort(404)
+
+    return jsonify([review.to_dict() for review in place.reviews])
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE'])
+def delete_amenity(place_id, amentiy_id):
+    """remote an Amenity object from a place based on id."""
+    place = storage.get("Place", place_id)
+    amentiy = storage.get("Amentiy", amentiy_id)
+    if place is None:
+        abort(404)
+    if amentiy is None:
+        abort(404)
+    if amentiy not in place.amenities:
+        abort(404)
+
+    place.amenities.remove(amenity)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'])
+def link_place_amenity(place_id, amenity_id):
+    """Addes an Amenity object from a place based on id."""
+    place = storage.get("Place", place_id)
+    amentiy = storage.get("Amentiy", amenity_id)
+    if place is None:
+        abort(404)
+    if amentiy is None:
+        abort(404)
+    if amentiy in place.amenities:
+        return jsonify(amentiy.to_dict()) 200
+
+    place.amenities.append(amenity)
+    storage.save()
+    return jsonify(amenity.to_dict()), 201
